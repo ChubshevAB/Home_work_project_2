6ec7a19e89dbc70348e9052d45b49270573b0673
Adm <anton.chubshev@yandex.ru>
2024-09-17 18:59:04 +0300

    fix code

--- src/products.py
+++ src/products.py
@@ -10,6 +10,13 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток {self.quantity} шт."
+
+    def __add__(self, other):
+        total_price = self.__price * self.quantity + other.__price * other.quantity
+        return total_price
+
     @property
     def price(self):
         return self.__price
